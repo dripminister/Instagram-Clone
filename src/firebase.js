06e8c3a109cc14e7f8c0bc8fa37c16bc8ef4939f
dripminister <95714820+dripminister@users.noreply.github.com>
2022-12-02 09:15:20 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-	//Firestore config goes here
+	//Firebase config goes here
 }
 
 const app = initializeApp(firebaseConfig)
